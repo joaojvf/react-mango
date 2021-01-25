7642b8cdd741a19f2f2cef0352be6710552092cd
joaojvf <joao.victorf02@hotmail.com>
2021-01-25 16:02:07 -0300

    feat: add all

--- src/a.spec.ts
+++ src/a.spec.ts
@@ -1,8 +1,5 @@
 describe('aa', () => {
-    test('aa', () => {
-        expect(1).toBe(1)
-    })
-    test('bb', () => {
-        expect(1).toBe(2)
-    })
+  test('aa', () => {
+    expect(1).toBe(1)
+  })
 })
